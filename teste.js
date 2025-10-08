d4ed50b6ebb5c86d85734422e9b40794b8cf93cc
Kaio Nascimento <kaio.seth@gmail.com>
2025-10-07 20:17:15 -0400

    upload structure

---
+++ teste.js
@@ -0,0 +1,13 @@
+describe("Função somarNumeros", function() {
+    it("deve somar todos os números do array", function() {
+        expect(somarNumeros([1,2,3])).to.equal(6);
+    });
+
+    it("deve retornar 0 para array vazio", function() {
+        expect(somarNumeros([])).to.equal(0);
+    });
+
+    it("deve lançar erro se entrada não for array", function() {
+        expect(() => somarNumeros("teste")).to.throw("Entrada inválida");
+    });
+});
