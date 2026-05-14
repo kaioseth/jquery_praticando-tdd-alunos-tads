daf6f96d6e783698582d27ba9fd1a73bd87ea5b8
Kaio Nascimento <kaio.seth@gmail.com>
2026-05-14 17:35:56 -0400

    update repo

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 function somarNumeros(li) {
-    
+    if (!Array.isArray(arr)) {
+        throw new Error("Entrada inválida");
+    }
+
+    arr.forEach(element => {
+        if (typeof element != 'number') {
+            return "Entrada inválida";
+        }
+    });
+
+    var soma = 0;
+
+    arr.forEach(element => {
+        soma += element;
+    });
+
+    return soma;
 }
